6649da1b4020b7119f9cf5644166550c666934f6
hansen <623229800@qq.com>
2018-06-28 09:16:49 +0800

    add oneline

--- src/main/java/com/yphone/controller/MainController.java
+++ src/main/java/com/yphone/controller/MainController.java
@@ -54,6 +54,7 @@ public class MainController {
     @RequestMapping(value = "/registerProcess",method = RequestMethod.POST)
     public String registerProcess(){
         //TODO 用户注册
+
         
         return "success";
     }
